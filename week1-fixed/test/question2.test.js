d422411d727c1953536d5d81b967c556169ca748
fataneh-mow <fataneh.mow@gmail.com>
2025-11-14 23:08:52 +0430

    question2.test.js modified for the last time isPrime function tests passed

--- week1-fixed/test/question2.test.js
+++ week1-fixed/test/question2.test.js
@@ -1,5 +1,7 @@
 import { it, describe , expect } from "vitest";
 import { isEven } from "../src/question2";
+import { isPrime } from "../src/question2";
+
 
 describe ("isEven()", () => {
     it("returns 'even' for the even numbers", () => {
@@ -15,4 +17,26 @@ describe ("isEven()", () => {
         expect(isEven(1)).toBe("odd");
         expect(isEven(35)).toBe("odd");
     })
+});
+
+describe("isPrime()", () => {
+    it("returns true for prime numbers", () => {
+        expect(isPrime(2)).toBe(true);
+        expect(isPrime(3)).toBe(true);
+        expect(isPrime(5)).toBe(true);
+        expect(isPrime(7)).toBe(true);
+    });
+
+    it("returns false for non-prime numbers", () => {
+        expect(isPrime(4)).toBe(false);
+        expect(isPrime(6)).toBe(false);
+        expect(isPrime(9)).toBe(false);
+        expect(isPrime(10)).toBe(false);
+    });
+
+    it("returns undefined for numbers < 2", () => {
+        expect(isPrime(1)).toBe(undefined);
+        expect(isPrime(0)).toBe(undefined);
+        expect(isPrime(-5)).toBe(undefined);
+    });
 });
