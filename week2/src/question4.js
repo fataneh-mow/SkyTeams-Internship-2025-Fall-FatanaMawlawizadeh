0cd3afaabf3c9a4d269ae2c1f50a6f9242fff569
fataneh-mow <fataneh.mow@gmail.com>
2025-11-18 15:56:37 +0430

    question4.js uploaded

---
+++ week2/src/question4.js
@@ -0,0 +1,34 @@
+/**
+    * Author: Fatana Mawlawizadeh
+     
+    * Date: Nov, 16th, 2025
+
+    * Question: Write a function to convert the tempreature
+
+    * F = (C × 9/5) + 32
+      C = (F - 32) × 5/9
+*/
+
+function convertToCelsius (temp) {
+    if (isNaN(temp)) {
+        return `Not a number!`;
+    }
+    else{
+        return Math.round((temp - 32) * 5 / 9);
+    }
+}
+
+function convertToFahrenheit  (temp) {
+    if (isNaN(temp)) {
+        return `Not a number!`;
+    } 
+    else {
+        return Math.round(((temp* 9/5) + 32));
+    }
+}
+
+let resultC = convertToCelsius(12);
+console.log(resultC);
+
+let resultF = convertToFahrenheit (34);
+console.log(resultF);
