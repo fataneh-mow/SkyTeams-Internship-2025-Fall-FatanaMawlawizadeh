e78c95fd7df11ba52b0c0521f824c1ba2a6a614e
fataneh-mow <fataneh.mow@gmail.com>
2025-11-18 18:10:24 +0430

    question5.test.js modified as requested

--- week1-fixed/test/question5.test.js
+++ week1-fixed/test/question5.test.js
@@ -7,5 +7,8 @@ describe("gratestNumber finds the biggest numbers in an array", () => {
         expect(greatestNumber([789, 0, 10.1])).toBe(789);
         expect(greatestNumber([4.1, -19, -18.54])).toBe(4.1);
         expect(greatestNumber([-43, -50, 0, -65])).toBe(0);
+        expect(greatestNumber([])).toBe(null);
+        expect(greatestNumber(null)).toBe(null);
+
     })
 })
