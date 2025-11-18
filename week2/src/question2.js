2321cc32db2edbe89489d67b4c916529fbc49807
fataneh-mow <fataneh.mow@gmail.com>
2025-11-18 21:14:38 +0430

    question2.js modified

--- week2/src/question2.js
+++ week2/src/question2.js
@@ -7,24 +7,20 @@
 
 */
 
-function minMax (a, b, c) {
-
+export function minMax (a, b, c) {
     items = [a, b, c];
 
     let max = 0;
     for (let i = 0; i < items.length; i++) {
         if (items[i] > max) {
             max = items[i];
-        }
-    }
-    console.log(maxx);
+        };
+    };
 
     let min = items[0];
     for ( i = 0; i < items.length; i++) {
         if (min > items[i]){
             min = items[i];
-        }
-    }
-    console.log(min);
-}
-minMax(1,2,3);
+        };
+    };
+};
