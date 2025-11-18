2ce2fbf97d5d0eafc1a9b869b0731307618609c3
fataneh-mow <fataneh.mow@gmail.com>
2025-11-18 17:41:29 +0430

    edge cases for testing added

--- week1-fixed/test/question3.test.js
+++ week1-fixed/test/question3.test.js
@@ -6,5 +6,9 @@ describe("arraySum must find the sum of an array", () => {
         expect(arraySum([2,4,5])).toBe(11);
         expect(arraySum([100, 100])).toBe(200);
         expect(arraySum([2,4,0])).toBe(6);
+        expect(arraySum([])).toBe(0);
+        expect(arraySum([-19, 19])).toBe(0);
+
+
     })
 });
