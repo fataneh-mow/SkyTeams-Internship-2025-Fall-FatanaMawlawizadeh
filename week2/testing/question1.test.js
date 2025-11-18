a87b14fff29639da9b84e27b53e864b2428a8914
fataneh-mow <fataneh.mow@gmail.com>
2025-11-18 20:39:50 +0430

    question1.test.js file added

---
+++ week2/testing/question1.test.js
@@ -0,0 +1,14 @@
+/**
+    * Author: Fatana Mawlawizadeh
+ 
+    * Date: Nov, 18th, 2025
+
+    * Question: Write a function that swaps two values, without using a temp variable
+
+    Example:
+    Input:
+    a = 5, b = 10
+    Output:
+    a = 10, b = 5
+
+ */
