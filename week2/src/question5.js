ed82e08a943fae17e83d362ed6dbc709b57f596d
fataneh-mow <fataneh.mow@gmail.com>
2025-11-19 08:57:19 +0430

    question5.js modified

--- week2/src/question5.js
+++ week2/src/question5.js
@@ -14,17 +14,13 @@
    
 */
 
-function simpleInterest (P , R, T) {
+export function simpleInterest (P , R, T) {
     let SI = (P * R * T ) / 100;
     return `SI = ${SI}`;
 };
 
 
-function compoundInterest (P , R, T) {
+export function compoundInterest (P , R, T) {
     let CI = Number((P * (Math.pow(1 + R/100, T) - 1)).toFixed(1)); 
     return `CI = ${CI}`;
 };
-
-let result = simpleInterest(1000, 5, 2);
-let result2 = compoundInterest(1000, 5, 2);
-console.log(result, result2);
