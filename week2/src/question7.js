6af81bd4313c1405ef01a37a82762384a445ea12
fataneh-mow <fataneh.mow@gmail.com>
2025-11-19 10:44:28 +0430

    question7.js and question7.test.js modified

---
+++ week2/src/question7.js
@@ -0,0 +1,29 @@
+/**
+    * Author: Fatana Mawlawizadeh
+     
+    * Date: Nov, 18th, 2025
+
+    * Question: Write a function to check wheteher a number is even or odd
+
+    Description: Determine if a number is divisible by 2. 
+    Example: 
+    Input: 
+    7 
+    Output: 
+    Odd
+*/
+
+export function typeNumber(input) {
+    const num = Number(input);
+
+    if (isNaN(num)) {
+        return `${input} is not checkable`;
+    }
+
+    if (num % 2 === 0) {
+        return `${num} is even`;
+    } else {
+        return `${num} is odd`;
+    }
+};
+
