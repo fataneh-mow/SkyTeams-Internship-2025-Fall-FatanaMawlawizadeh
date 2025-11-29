302f31129a8389150d4140c82480d6978ed5a30b
fataneh-mow <fataneh.mow@gmail.com>
2025-11-29 12:44:44 +0430

    question21.js added

---
+++ week3/src/question21.js
@@ -0,0 +1,22 @@
+/*
+    * Author: Fatana Mawlawizadeh *
+
+    * Date: Nov, 25th, 2025 *
+
+    Question 21. Password Strength Checker 
+    Description: A strong password must: 
+    • Be at least 8 characters long. 
+    • Contain at least one uppercase letter, one lowercase letter, and one number. 
+    Example: 
+    Input: 
+    MyPass123 
+    Output: 
+    Strong 
+    Input: 
+    pass 
+    Output: 
+    Weak
+
+*/
+
+// export function 
