d2681dd5a1fd2034cb3118a22359484a14fe4220
fataneh-mow <fataneh.mow@gmail.com>
2025-11-29 17:58:30 +0430

    question21.js added

--- week3/src/question21.js
+++ week3/src/question21.js
@@ -19,4 +19,38 @@
 
 */
 
-// export function 
+export function passwordStrengthChecker (password) {
+    if (typeof password !== 'string' || password === undefined || password === null) {
+        throw new Error(`Error: Invalid input
+        Make sure input is not undefined or null`);
+    }
+
+    let passStrength = "";
+        
+    if (password.length < 8) { passStrength = "Weak" }
+
+    let letters = [];
+    for (let i = 65; i <= 90; i++) {
+        letters.push(String.fromCharCode(i));
+    }
+
+    let smallcaseLeters = [];
+    for (const item of letters) {
+        smallcaseLeters.push(item.toLowerCase())
+    }
+
+    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    for (let i = 0; i < password.length; i++) {
+
+        if (letters.some( letter => password.includes(letter))
+            && smallcaseLeters.some( smallletter => password.includes(smallletter)
+            && numbers.some( num => password.includes(num)))) {
+            passStrength = "Strong"
+        }
+        else {
+            passStrength = "Weak"
+        }
+    }
+    return passStrength;
+};
