4c9d1423885306ae6b57776bf40a64d3f34f128f
fataneh-mow <fataneh.mow@gmail.com>
2025-12-01 13:26:27 +0430

    question27.js and testing added

---
+++ week3/src/question27.js
@@ -0,0 +1,37 @@
+/*
+    * Author: Fatana Mawlawizadeh *
+
+    * Date: Nov, 25th, 2025 *
+
+    Question 27. Find Unique Elements in an Array 
+    Description: Return an array with only unique elements from the input array. 
+    Example: 
+    Input: 
+    [1, 2, 2, 3, 4, 4, 5] 
+    Output: 
+    [1, 2, 3, 4, 5]
+
+*/
+
+export function uniqueElements (array) {
+    if (!Array.isArray(array) || array === null || array === undefined) {
+        throw new Error(`Error: Invalid input
+        Make sure input is Array`);
+    }
+
+    let uniqueArr = []
+
+    if (array.length === 0) {
+        uniqueArr = [];
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (!uniqueArr.includes(array[i])) {
+            uniqueArr.push(array[i]);
+        }
+    }
+
+    return uniqueArr;
+};
+console.log(uniqueElements([1,1,2,3,3,3,4]));
+console.log(uniqueElements([1,1,2]));
