a91d4ccf0e7f3f9d53f705c464e9a4a728066fa9
fataneh-mow <fataneh.mow@gmail.com>
2025-12-05 20:14:11 +0430

    question11.js initialized

---
+++ week4/src/question11.js
@@ -0,0 +1,11 @@
+/**
+    Author: Fatana Mawlawizadeh
+ 
+    Date: Dec, 5nd, 2025
+
+    Question 11. String Rotation Check 
+    Description: Check if one string is rotation of another. 
+    Example: 
+    "waterbottle", "erbottlewat" → True
+
+*/
