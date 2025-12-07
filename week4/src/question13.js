86c34aeaac6bbc0b1f1201d5e9b7d8a8cf90df5c
fataneh-mow <fataneh.mow@gmail.com>
2025-12-07 16:25:10 +0430

    question14.js added

---
+++ week4/src/question13.js
@@ -0,0 +1,14 @@
+/**
+    Author: Fatana Mawlawizadeh
+ 
+    Date: Dec, 5nd, 2025
+
+    Question 13. String Pattern Matching ("abba") 
+    Description: Check if sentence follows the pattern. 
+    Example: "dog cat cat dog" → True
+
+*/
+
+// export function stringPatternMatching (abba) {
+    // if (typeof )
+// }
