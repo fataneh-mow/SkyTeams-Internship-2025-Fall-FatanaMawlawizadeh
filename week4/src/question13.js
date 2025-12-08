ea528ccabdc5e432e6fa73e90ea7bd2c13044d2b
fataneh-mow <fataneh.mow@gmail.com>
2025-12-08 12:10:05 +0430

    Question 13 modified and its testing added

--- week4/src/question13.js
+++ week4/src/question13.js
@@ -9,6 +9,76 @@
 
 */
 
-// export function stringPatternMatching (abba) {
-    // if (typeof )
-// }
+// export function isStringPatternMatching (str) {
+//     if (typeof str !== "string" || str === null || str === undefined) {
+//         throw new Error(`Error: Ivalid input
+//         Make sure input is string`);
+//     } 
+//     let splitedStr = str.split('');
+
+//     let startIndex = splitedStr[0];
+
+//     let endChar = splitedStr.length -1
+//     let endIndex = splitedStr[endChar]
+
+//     let isPatternMatching = true;
+
+//     while (startIndex <= endIndex) {
+
+//         if (startIndex !== endIndex) {
+//             isPatternMatching = false;
+//         }
+
+//         startIndex += 1;
+//         endIndex -= 1;
+//     }
+
+//     return isPatternMatching;
+// };
+
+
+export function isPatternMatchingString (pattern, str) {
+    if (typeof pattern !== "string" || pattern === null || pattern === undefined || typeof str !== "string" || str === null || str === undefined) {
+        throw new Error(`Error: Ivalid inputs
+        Make sure inputs are string`);
+    }
+
+    let isPatternMatching = false;
+
+    let characters;
+
+    if (str.includes(' ')) {
+        characters = str.split(' ')
+    } else {
+        characters = str.split('')
+    }
+
+    const map = new Map();
+    const usedCharacters = new Set();
+
+    for (let i = 0; i < pattern.length; i++) {
+        let patternCharacter = pattern[i];
+        let strCharacter = characters[i];  
+        
+        if (!map.has(patternCharacter)) {
+            if (usedCharacters.has(strCharacter)) {
+                return isPatternMatching;
+            }
+            else {
+                map.set(patternCharacter, strCharacter);
+                usedCharacters.add(strCharacter);
+            }
+        }
+        else {
+            if (map.get(patternCharacter) !== strCharacter) {
+                return isPatternMatching;
+            }
+        }
+    }
+    isPatternMatching = true;
+    return isPatternMatching;
+}
+
+console.log(isPatternMatchingString("abba","dog cat cat dog"));
+console.log(isPatternMatchingString("abab","mama"));
+console.log(isPatternMatchingString("abca","dog cat cat dog"));
