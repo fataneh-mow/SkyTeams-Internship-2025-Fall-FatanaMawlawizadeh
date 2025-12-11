c5fe4a2de3b866fca037767805f3e37054a52475
fataneh-mow <fataneh.mow@gmail.com>
2025-12-11 19:58:14 +0430

    Questions 21 and 22 initialized

---
+++ week4/src/question21.js
@@ -0,0 +1,10 @@
+/**
+    Author: Fatana Mawlawizadeh
+ 
+    Date: Dec, 11th, 2025
+
+    Question 21. Longest Palindromic Substring 
+    Input: "babad" → "bab" or "aba" 
+
+*/
+
---
+++ week4/src/question22.js
@@ -0,0 +1,9 @@
+/**
+    Author: Fatana Mawlawizadeh
+ 
+    Date: Dec, 11th, 2025
+
+    Question 22. Merge Overlapping Intervals 
+    Input: [[1,3],[2,6],[8,10]] → [[1,6],[8,10]] 
+
+*/
