0940a264dcfcb6d48412689e9d458fc5b0fc4f68
fataneh-mow <fataneh.mow@gmail.com>
2025-12-11 22:11:50 +0430

    question21.js and its testing added

--- week4/src/question21.js
+++ week4/src/question21.js
@@ -8,3 +8,41 @@
 
 */
 
+import { utilitiesReverseString, utilitiesGenerateSubStringsOfString } from "../utilities/utilities.js";
+
+export function longestPalindromicSubstring (str) {
+    if (typeof str !== "string" || str === null || str === undefined) {
+        throw new Error(`Error: Invalid Input
+        Make sure input is string`);
+    }
+
+    if (str.length === 0) {
+        return str
+    }
+    
+    let subStrings = utilitiesGenerateSubStringsOfString(str);
+
+    // reversing each sub string
+    let reversed = [];
+
+    for (let i = 0; i < subStrings.length; i++) {
+        reversed.push(utilitiesReverseString(subStrings[i]))
+    }
+
+    let longestPalinSubstring = "";
+
+    for (let i = 0; i < subStrings.length; i++) {
+        let originalStr = subStrings[i]
+        let reversedStr = reversed[i]
+
+        if (originalStr === reversedStr) {
+            if (originalStr.length > longestPalinSubstring.length) {
+                longestPalinSubstring = originalStr
+            }
+        }
+    }
+
+    return longestPalinSubstring;
+}
+
+console.log(longestPalindromicSubstring("babad"))
