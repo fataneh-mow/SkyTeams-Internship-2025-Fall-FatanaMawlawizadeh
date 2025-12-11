4bbf30be77fa59c1d6541f83ae53ea852b0feafe
fataneh-mow <fataneh.mow@gmail.com>
2025-12-11 12:48:37 +0430

    question3.js modified

--- week4/src/question3.js
+++ week4/src/question3.js
@@ -25,10 +25,16 @@ export function rotateArrayToRight (arr, times) {
     }
 
     for (let i = 0; i < times; i++) {
-        let last = arr[arr.length -1];
-        arr.pop();
-        arr.unshift(last);
+        let lastIndex = arr[ arr.length -1 ];
+
+        for (let j = arr.length - 1; j > 0; j--) {
+            arr[j] = arr[j -1]
+        }     
+
+        arr[0] = lastIndex
     }
     
     return arr
 };
+
+console.log(rotateArrayToRight([1, 2, 3], 2)) //[2, 3, 1]
