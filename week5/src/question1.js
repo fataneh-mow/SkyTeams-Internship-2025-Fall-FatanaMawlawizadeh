6c3aefc27626d8424663bf6bd8677d6a10b57617
fataneh-mow <fataneh.mow@gmail.com>
2025-12-13 11:25:43 +0430

    Week 5 initialized

---
+++ week5/src/question1.js
@@ -0,0 +1,20 @@
+/*
+    * Author: Fatana Mawlawizadeh *
+
+    * Date: Dec, 13th, 2025 *
+
+    Part 1 — Linked List Basics  
+    1. Create a Singly Linked List 
+        • Create a linked list with 3 nodes [10, 20, 30] and print all elements. 
+    2. Add a Node at the Beginning 
+        • Insert 5 at the start → [5,10,20,30]. 
+    3. Add a Node at the End 
+        • Append 40 → [5,10,20,30,40]. 
+    4. Delete the First Node 
+        • Remove head → [10,20,30,40]. 
+    5. Search for a Value 
+        • Check if 20 exists → return true/false.
+
+*/
+
+// export function 
