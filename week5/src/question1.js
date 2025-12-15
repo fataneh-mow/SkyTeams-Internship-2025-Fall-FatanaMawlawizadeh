1d5a4d8f474543b57faa2cfe8911150e2eb8706c
fataneh-mow <fataneh.mow@gmail.com>
2025-12-15 21:16:08 +0430

    question1.js modified

--- week5/src/question1.js
+++ week5/src/question1.js
@@ -1,7 +1,7 @@
 /*
     * Author: Fatana Mawlawizadeh *
 
-    * Date: Dec, 13th, 2025 *
+    * Date: Dec, 14th, 2025 *
 
     Part 1 — Linked List Basics  
     1. Create a Singly Linked List 
@@ -17,4 +17,57 @@
 
 */
 
-// export function 
+// print helper function
+export function printSinglyLinkedList (head) {
+    let current = head
+    let values = []
+    while (current !== null) {
+        values.push(current.value)
+        current = current.next
+    }   
+    return values
+}
+
+// 1. Create a Singly Linked List 
+// • Create a linked list with 3 nodes [10, 20, 30] and print all elements. 
+export function createSinglyLinkedList (element1, element2, element3) {
+    let node1 = {
+        value: element1,
+        next: null
+    }
+    let node2 = {
+        value: element2,
+        next: null
+    }
+    let node3 = {
+        value: element3,
+        next: null
+    }
+
+    // linking nodes together
+    node1.next = node2
+    node2.next = node3
+
+    let printed =  printSinglyLinkedList(node1)
+    
+    return printed
+}
+
+// console.log(createSinglyLinkedList(10, 20, 30))
+
+
+// 2. Add a Node at the Beginning 
+// • Insert 5 at the start → [5,10,20,30]. 
+
+// export function addNodeAtTheBeginning (element) {
+//     let head = createSinglyLinkedList(10, 20, 30)
+
+//     let newNode = {
+//         value: element,
+//         next : head
+//     }
+
+//     let printed = printSinglyLinkedList(newNode)
+//     return printed
+// }
+// console.log(addNodeAtTheBeginning(5))
