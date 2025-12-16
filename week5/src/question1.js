ba44c95c4d395389b441f7e6c5fc91751e91a118
fataneh-mow <fataneh.mow@gmail.com>
2025-12-16 17:22:11 +0430

    question1.js modified and its testing added

--- week5/src/question1.js
+++ week5/src/question1.js
@@ -31,6 +31,11 @@ export function printSinglyLinkedList (head) {
 // 1. Create a Singly Linked List 
 // • Create a linked list with 3 nodes [10, 20, 30] and print all elements. 
 export function createSinglyLinkedList (element1, element2, element3) {
+    if (element1 === null || element1 === undefined || element2 === null || element2 === undefined || element3 === null || element3 === undefined) {
+        throw new Error(`Error: Invalid inputs
+        Make sure inputs are not empty or undefined`);
+    }
+
     let node1 = {
         value: element1,
         next: null
@@ -61,8 +66,11 @@ export function createSinglyLinkedList (element1, element2, element3) {
 // 2. Add a Node at the Beginning 
 // • Insert 5 at the start → [5,10,20,30]. 
 
-export function addNodeAtTheBeginning (element) {
-    let head = createSinglyLinkedList(10, 20, 30)
+export function addNodeAtTheBeginning (head, element) {
+    if (element === null || element === undefined || head === null || head === undefined) {
+        throw new Error(`Error: Invalid input
+        elemnt can't have a value of null or undefined`);
+    }
 
     let newNode = {
         value: element,
@@ -71,15 +79,16 @@ export function addNodeAtTheBeginning (element) {
 
     return newNode
 }
-
-// let nodeAdded = addNodeAtTheBeginning(5)
-// console.log(nodeAdded)
-// console.log(printSinglyLinkedList(nodeAdded))
+// let myLinked = createSinglyLinkedList(10, 20, 30)
+// console.log(printSinglyLinkedList(addNodeAtTheBeginning(myLinked, 5)))
 
 //  3. Add a Node at the End 
 // • Append 40 → [5,10,20,30,40].
-export function addNodeAtTheEnd (element) {
-    let head = addNodeAtTheBeginning(5)
+export function addNodeAtTheEnd (head, element) {
+    if (element === null || element === undefined || head === null || head === undefined) {
+        throw new Error(`Error: Invalid input
+        elemnt can't have a value of null or undefined`);
+    }
 
     let newNode = {
         value: element,
@@ -96,24 +105,32 @@ export function addNodeAtTheEnd (element) {
     return head
 }
 
-// console.log(printSinglyLinkedList(addNodeAtTheEnd(40)))
-
 // 4. Delete the First Node 
 // • Remove head → [10,20,30,40]
-export function deleteFirstNode (linkedList) {
-    let removed = linkedList.next
+export function deleteFirstNode (head) {
+    if (head === null || head === undefined) {
+        throw new Error(`Error: Invalid input
+        head must not be null or undefined`);
+    }
+
+    let removed = head.next
     return removed
 }
 
-let myLinledList = createSinglyLinkedList(1, 2, 3)
-myLinledList = deleteFirstNode(myLinledList)
-myLinledList = deleteFirstNode(myLinledList)
+// let myLinledList = createSinglyLinkedList(1, 2, 3)
+// myLinledList = deleteFirstNode(myLinledList)
+// myLinledList = deleteFirstNode(myLinledList)
 
-console.log(printSinglyLinkedList(myLinledList))
+// console.log(printSinglyLinkedList(myLinledList))
 
 // 5. Search for a Value 
 // • Check if 20 exists → return true/false.
 export function isSearchForValue (head, value) {
+    if (head === null || head === undefined || value === null || value === undefined) {
+        throw new Error(`Error: Invalid inputs
+        inputs must not be null or undefined`);
+    }
+
     while (head.value !== null) {
         if(head.value === value) {
             return true
