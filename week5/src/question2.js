a3026633f6242e82f9b24026eb31cbba2769770c
fataneh-mow <fataneh.mow@gmail.com>
2025-12-16 17:26:10 +0430

    question2.js initialized

---
+++ week5/src/question2.js
@@ -0,0 +1,20 @@
+/*
+    * Author: Fatana Mawlawizadeh *
+
+    * Date: Dec, 16th, 2025 *
+
+    Part 2 — Queue Basics Using Linked Lists 
+    1. Implement a Queue 
+        • Use a linked list to create a queue class with enqueue, dequeue, front, rear, isEmpty. 
+    2. Enqueue and Dequeue 
+        • Enqueue [10,20,30], dequeue 1 element, print the queue. 
+    3. Check if Queue is Empty 
+        • Test before and after enqueueing items. 
+    4. Get Front and Rear Values 
+        • Print the front and rear of a queue after adding [5,15,25]. 
+    5. Reverse a Small Queue 
+        • Reverse a queue [1,2,3] using a temporary stack/array
+
+*/
+
+// export function
