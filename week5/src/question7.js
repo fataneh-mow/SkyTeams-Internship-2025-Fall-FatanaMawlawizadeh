7f512b798ed0803e93e9b7466afef0cef73e9acf
fataneh-mow <fataneh.mow@gmail.com>
2025-12-18 22:15:48 +0430

    src/question7.js added

---
+++ week5/src/question7.js
@@ -0,0 +1,15 @@
+/*
+    * Author: Fatana Mawlawizadeh *
+
+    * Date: Dec, 17th, 2025 *
+
+    Question 3. Implement Queue Using Stacks 
+    Problem: 
+    Implement a queue using two stacks. 
+    Operations: 
+    • push(x) 
+    • pop() → removes front 
+    • peek() → returns front 
+    • empty()     
+
+*/
