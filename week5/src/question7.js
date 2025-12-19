e370a876301e542d6acf885deb0987f87f808ab1
fataneh-mow <fataneh.mow@gmail.com>
2025-12-19 21:51:09 +0430

    question7.js modified

--- week5/src/question7.js
+++ week5/src/question7.js
@@ -3,13 +3,57 @@
 
     * Date: Dec, 17th, 2025 *
 
-    Question 3. Implement Queue Using Stacks 
+    Question 4. Evaluate Reverse Polish Notation (RPN) 
     Problem: 
-    Implement a queue using two stacks. 
-    Operations: 
-    • push(x) 
-    • pop() → removes front 
-    • peek() → returns front 
-    • empty()     
+    Evaluate an expression in Reverse Polish Notation. 
+    Valid operators: + - * /. 
+    Example: 
+    Input: ["2","1","+","3","*"] 
+    Output: 9 
+    Explanation: (2 + 1) * 3 
 
 */
+import { Stack } from "./question5.js"
+
+export function reversePolishNotation (arr) {
+    if (!Array.isArray(arr) || arr === undefined || arr === null || arr.length === 0) {
+        throw new Error(`Error: Invalid input
+        Make sure input is an array of numbers and oprators`);
+    }
+
+    let stack = new Stack()
+    let result
+
+    for (let i = 0; i <arr.length; i++) {
+        let num = arr[i]
+
+        if (!isNaN(Number(num))) {
+            stack.push(Number(num))
+        }
+
+        else {
+            let b = stack.pop()
+            let a = stack.pop()
+
+            if (num === '+') {
+                result = a + b
+            }
+            else if (num === '-') {
+                result = a - b
+            }
+            else if (num === '*') {
+                result = a * b
+            }
+            else if (num === '/') {
+                result = a / b
+            }
+
+            stack.push(result)
+        }
+
+    }
+
+    return stack.pop()
+}
+
+console.log(reversePolishNotation(['1','2', '+']))
