feaffbd9fcc82bb424fa3573522859ac04f8f436
fataneh-mow <fataneh.mow@gmail.com>
2025-12-22 15:30:06 +0430

    question9.test.js added

---
+++ week5/testings/question9.test.js
@@ -0,0 +1,27 @@
+/*
+    * Author: Fatana Mawlawizadeh *
+
+    * Date: Dec, 20th, 2025 *
+
+    Question 6. Remove Adjacent Duplicates 
+    Problem: 
+    Given a string s, repeatedly remove adjacent pairs of equal characters. 
+    Example: 
+    Input: "abbaca" 
+    Output: "ca"
+
+*/ 
+
+import { describe, it, expect } from "vitest";
+import { removeAdjacentDuplicates } from "../src/question9";
+
+
+describe("removeAdjacentDuplicates fnction removes all duplicates of a string and returns a new string", () => {
+    it("Removes duplicates and returns it", () => {
+        expect(removeAdjacentDuplicates('1abcccc')).toBe('1abc')
+        expect(removeAdjacentDuplicates('abcc')).toBe('abc')
+        expect(removeAdjacentDuplicates('12333')).toBe('123')
+        expect(removeAdjacentDuplicates('000')).toBe('0')
+        expect(removeAdjacentDuplicates('$$$')).toBe('$')
+    });
+});
