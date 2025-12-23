75709312210ee14b33c1a064614b54129e678ee6
fataneh-mow <fataneh.mow@gmail.com>
2025-12-23 20:41:25 +0430

    question9.js new solution implemented

--- week5/src/question9.js
+++ week5/src/question9.js
@@ -20,49 +20,18 @@ export function removeAdjacentDuplicates (str) {
         Make sure input is string`);
     }
 
-    let stackStr = new Stack()
-    let removedDuplicates = ''
-
+    let stack = new Stack()
 
     for (let i = 0; i < str.length; i++) {
-        // let current = str[i]
+        let current = str[i]
 
-        // if (!stackStr.isEmpty() && stackStr.peek() !== current) {
-        //     stackStr.push(current)
-        // }
+         if (!stack.isEmpty() && stack.peek() === current) {
+            stack.pop();
+        } else {
+            stack.push(current);
+        }
     }
-
-    // let stackStrPrinted = stackStr.printToArray()
-    
-    // for (let j = 0; j < stackStrPrinted.length; j++) {
-        // if (!utilitiesInlcludes(removedDuplicates, stackStrPrinted[j])) {
-            // removedDuplicates += (stackStrPrinted[j])
-        // }
-    // }
-
-    // removedDuplicates = utilitiesReverseString(removedDuplicates)
-    // return removedDuplicates
-    return stackStr
+    return stack.printToArray().join('')
 }
 
 console.log(removeAdjacentDuplicates('abccc'))
-
-function utilitiesInlcludes (str, target) {
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === target) {
-            return true
-        }
-    }
-    return false
-}
-
-
-function utilitiesReverseString (str) {
-    let reversed = '';
-
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i] ;
-    }
-
-    return reversed
-}
