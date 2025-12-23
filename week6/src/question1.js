ebf36e29463a6b1787829d92a6bd7a5dcad4abff
fataneh-mow <fataneh.mow@gmail.com>
2025-12-23 20:47:34 +0430

    week6 directory initialized

---
+++ week6/src/question1.js
@@ -0,0 +1,10 @@
+/*
+    * Author: Fatana Mawlawizadeh *
+
+    * Date: Dec, 24th, 2025 *
+
+    Question 1. Height of Binary Tree 
+    Write a function that returns the number of levels in a binary tree. 
+    An empty tree has height 0, and a tree with only one node has height 1
+
+*/
